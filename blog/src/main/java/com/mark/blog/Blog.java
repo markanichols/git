bb3b924ecc496cd0f2b4cc0ad319c314645f0239
Mark Nichols <mark.nichols@tesco.net>
2014-02-12 00:07:30 +0000

    Initial Version

---
+++ blog/src/main/java/com/mark/blog/Blog.java
@@ -0,0 +1,50 @@
+package com.mark.blog;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Blog {
+	
+	private String id = UUID.randomUUID().toString();
+	
+	@NotBlank
+	private String title;
+	
+	@URL
+	@NotBlank
+	private String url;
+	
+	private final Date publishedOn = new Date();
+	
+	public Blog() {
+	}
+	
+	public Blog(String title, String url) {
+		super();
+		this.title = title;
+		this.url = url;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public Date getPublishedOn() {
+		return publishedOn;
+	}
+
+}
---
+++ blog/src/main/java/com/mark/blog/BlogConfiguration.java
@@ -0,0 +1,26 @@
+package com.mark.blog;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.yammer.dropwizard.config.Configuration;
+
+public class BlogConfiguration extends Configuration{
+	
+	@JsonProperty
+	@NotEmpty
+	public String mongohost = "localhost";
+	
+	@JsonProperty
+	@Min(1)
+	@Max(65535)
+	public int mongoport = 27017;
+	
+	@JsonProperty
+	@NotEmpty
+	public String mongodb = "Blog";
+
+}
